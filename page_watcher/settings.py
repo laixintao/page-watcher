0d6d4e828ccd7f137b98ab55fe2a45eb7396f83c
laixintao <glenn.lai@bosondata.com.cn>
2018-04-03 12:25:14 +0800

    setup postgres database

--- page_watcher/settings.py
+++ page_watcher/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'page_watcher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'page_watcher',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
